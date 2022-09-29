5d799bc2b2be7b79f8f72da08287aa95ebc4a729
Nasser <suwailah@umich.edu>
2022-09-29 17:19:30 -0400

    commit 1

--- Hello.java
+++ Hello.java
@@ -1,9 +1,6 @@
-public class Hello
-{
-public static void main(String[] args)
-{
-for(int i=1;i<=11;i++)
-{
+public class Hello {
+public static void main(String[] args) {
+for(int i=1;i<=9;i++) {
 System.out.println("Hello World..."+i);
 }
 }
